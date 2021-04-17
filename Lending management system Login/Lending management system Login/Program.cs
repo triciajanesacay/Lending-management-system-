0b2c7a17ddf2a33dd44de9ac14d88210969b31b6
triciajanesacay <81967275+triciajanesacay@users.noreply.github.com>
2021-04-17 22:50:14 +0800

    First commit

---
+++ Lending management system Login/Lending management system Login/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(String []
+        args)
+            {
+            Console.WriteLine("Create New User");
+
+            String username, password = string.Empty;
+
+            Console.Write("Enter a username");
+            username = Console.ReadLine();
+            Console.Write("Enter a password");
+            password = Console.ReadLine();
+
+            using (StreamWriter sw = new StreamWriter(File.Create("C:\\1.txt")))
+            {
+                sw.Write(username);
+                sw.Write(password);
+                sw.Close ();
+
+                Console.WriteLine("Done");
+
+            Console.Read();
+            }
+        }
+    }
+}

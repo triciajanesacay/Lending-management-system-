6b2b7033ee3d7b042995a01626521c0e5a7495aa
triciajanesacay <81967275+triciajanesacay@users.noreply.github.com>
2021-05-05 00:17:23 +0800

    Second commit

--- Lending management system Login/Lending management system Login/Program.cs
+++ Lending management system Login/Lending management system Login/Program.cs
@@ -4,32 +4,77 @@ using System.Linq;
 using System.Text;
 using System.IO;
 
-namespace Login
+namespace LoginSystem
 {
-    class Program
+    class program
     {
-        static void Main(String []
-        args)
-            {
-            Console.WriteLine("Create New User");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Personal Loans");
 
-            String username, password = string.Empty;
+            List<string> Loans = new List<string>(5);
+            Loans.Add("Two thousand Pesos");
+            Loans.Add("Five hundred Pesos");
+            Loans.Add("One thousand Pesos");
+            Loans.Add("One thousand Pesos");
+            Loans.Add("Four thousandPesos");
+            Loans.Add("Three thousand Pesos");
+            Loans.Add("Ten thousand Pesos");
+            Console.WriteLine("Original List Loans");
+            Console.WriteLine("===============");
 
-            Console.Write("Enter a username");
+            foreach (string a in Loans)
+                NewMethod(a);
+
+            Loans.Sort();
+            Console.WriteLine();
+            Console.WriteLine("Sorted List of Loans");
+            Console.WriteLine("===============");
+
+            foreach (string a in Loans)
+                Console.WriteLine(a);
+
+
+
+
+            Console.WriteLine("Lending Mangement System Log In ");
+
+            String username, password, username1, password1 = string.Empty;
+
+            Console.Write("Enter a username: ");
             username = Console.ReadLine();
-            Console.Write("Enter a password");
+            Console.Write("Enter a password: ");
             password = Console.ReadLine();
 
-            using (StreamWriter sw = new StreamWriter(File.Create("C:\\1.txt")))
+            using (StreamReader sr = new StreamReader(File.Open("C:\\1.txt", FileMode.Open)))
             {
-                sw.Write(username);
-                sw.Write(password);
-                sw.Close ();
+                username1 = sr.ReadLine();
+                password1 = sr.ReadLine();
+                sr.Close();
+            }
+            if
+                (username == username1 && password == password1) ;
+            {
+                Console.WriteLine("Login Succeful");
 
-                Console.WriteLine("Done");
 
-            Console.Read();
+
+            else
+                {
+                    Console.WriteLine("Login Failed");
+
+
+                    Console.Read();
+
+
+                }
             }
         }
+
+        private static void NewMethod(string a)
+        {
+            Console.WriteLine(a);
+        }
+
     }
-}
+

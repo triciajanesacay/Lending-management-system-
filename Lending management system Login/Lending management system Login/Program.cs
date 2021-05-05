67bb81e75ddad43849cc3ac701270c351d2ed81e
triciajanesacay <81967275+triciajanesacay@users.noreply.github.com>
2021-05-05 15:41:37 +0800

    second commit updated

--- Lending management system Login/Lending management system Login/Program.cs
+++ Lending management system Login/Lending management system Login/Program.cs
@@ -8,73 +8,56 @@ namespace LoginSystem
 {
     class program
     {
-        static void Main(string[] args)
+
+        static void Main(String[]
+       args)
         {
-            Console.WriteLine("Personal Loans");
+            Console.WriteLine("Log In ");
 
-            List<string> Loans = new List<string>(5);
-            Loans.Add("Two thousand Pesos");
-            Loans.Add("Five hundred Pesos");
-            Loans.Add("One thousand Pesos");
-            Loans.Add("One thousand Pesos");
-            Loans.Add("Four thousandPesos");
-            Loans.Add("Three thousand Pesos");
-            Loans.Add("Ten thousand Pesos");
-            Console.WriteLine("Original List Loans");
-            Console.WriteLine("===============");
+            String username, password = string.Empty;
 
-            foreach (string a in Loans)
-                NewMethod(a);
-
-            Loans.Sort();
-            Console.WriteLine();
-            Console.WriteLine("Sorted List of Loans");
-            Console.WriteLine("===============");
-
-            foreach (string a in Loans)
-                Console.WriteLine(a);
-
-
-
-
-            Console.WriteLine("Lending Mangement System Log In ");
-
-            String username, password, username1, password1 = string.Empty;
-
-            Console.Write("Enter a username: ");
+            Console.Write("Enter a username : ");
             username = Console.ReadLine();
-            Console.Write("Enter a password: ");
+            Console.Write("Enter a password :  ");
             password = Console.ReadLine();
 
-            using (StreamReader sr = new StreamReader(File.Open("C:\\1.txt", FileMode.Open)))
+           
+
             {
-                username1 = sr.ReadLine();
-                password1 = sr.ReadLine();
-                sr.Close();
-            }
-            if
-                (username == username1 && password == password1) ;
-            {
-                Console.WriteLine("Login Succeful");
+
+                Console.WriteLine("Personal Loans");
+
+                List<string> Loans = new List<string>(5);
+                Loans.Add("Two thousand Pesos");
+                Loans.Add("Five hundred Pesos");
+                Loans.Add("One thousand Pesos");
+                Loans.Add("One thousand Pesos");
+                Loans.Add("Four thousandPesos");
+                Loans.Add("Three thousand Pesos");
+                Loans.Add("Ten thousand Pesos");
+                Console.WriteLine("Original List Loans");
+                Console.WriteLine("===============");
+
+                foreach (string a in Loans)
+                    NewMethod(a);
+
+                Loans.Sort();
+                Console.WriteLine();
+                Console.WriteLine("Sorted List of Loans");
+                Console.WriteLine("===============");
+
+                foreach (string a in Loans)
+                    Console.WriteLine(a);
 
 
 
-            else
+                static void NewMethod(string a)
                 {
-                    Console.WriteLine("Login Failed");
-
-
-                    Console.Read();
-
-
+                    Console.WriteLine(a);
                 }
+
             }
         }
-
-        private static void NewMethod(string a)
-        {
-            Console.WriteLine(a);
-        }
-
     }
+}
 
